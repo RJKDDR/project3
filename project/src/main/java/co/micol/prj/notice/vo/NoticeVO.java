6b9417770f68c2ecd9f2e1afc5342b8754964865
kokoa <sat2633@naver.com>
2022-02-07 16:18:01 +0900

    02.01.16:17

---
+++ project/src/main/java/co/micol/prj/notice/vo/NoticeVO.java
@@ -0,0 +1,16 @@
+package co.micol.prj.notice.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NoticeVO {
+
+	private int noticeId;
+	private String noticeWriter;
+	private String noticeTitle;
+	private String noticeSubject;
+	private String noticeDate;
+	private int noticeHit;
+}
